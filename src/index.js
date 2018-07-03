42f661c3e86a04852c2fb7999f55cfc0af2cc259
yrodrigez <yrodrigez@gmail.com>
2018-07-03 13:16:37 +0100

    App up and running

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './config/routes';
 
-ReactDOM.render(routes, document.getElementById('src'));
+ReactDOM.render(routes, document.getElementById('app'));
