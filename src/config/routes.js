b77940dac8b9e6c5238d3215c1155cb494b6f438
yrodrigez <yrodrigez@gmail.com>
2018-07-03 12:46:30 +0100

    App up and running

---
+++ src/config/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {HashRouter as Router, Route,} from 'react-router-dom';
+import MainContainer from '../containers/Main/MainContainer';
+
+const routes = (
+  <Router>
+    <Route exact path="/" component={MainContainer}/>
+  </Router>
+);
+
+export default routes;
---
+++ src/containers/Main/MainContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class MainContainer extends Component {
+
+  render() {
+    return <p>Hello world</p>;
+  }
+}
+
+export default MainContainer;
